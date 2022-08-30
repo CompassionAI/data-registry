ae8e187149322b200cf2926090ecb556c2fc38ef
eisene <eeisenst@gmail.com>
2022-08-30 16:10:51 -0400

    Remove stray comments

--- push.sh
+++ push.sh
@@ -9,11 +9,11 @@ fi
 
 NOCOLOR='\033[0m' GREEN='\033[0;32m' echo -e "${GREEN}Pushing DVC cache${NOCOLOR}"
 cd ./raw_datasets/
-# dvc push
+dvc push
 cd ..
 echo -e "${GREEN}Pushing champion models${NOCOLOR}"
-# aws s3 sync ./champion_models/ s3://compassionai/data-registry/champion_models/
+aws s3 sync ./champion_models/ s3://compassionai/data-registry/champion_models/
 echo -e "${GREEN}Pushing processed datasets${NOCOLOR}"
-# aws s3 sync ./processed_datasets/ s3://compassionai/data-registry/processed_datasets/
+aws s3 sync ./processed_datasets/ s3://compassionai/data-registry/processed_datasets/
 echo -e "${GREEN}Pushing training results${NOCOLOR}"
-# aws s3 sync ./training_results/ s3://compassionai/data-registry/training_results/
+aws s3 sync ./training_results/ s3://compassionai/data-registry/training_results/
