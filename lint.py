54e27534caf03994335fed14278e08d90bc8c899
eisene <eeisenst@gmail.com>
2022-08-30 15:57:57 -0400

    Lint check for extra cards

--- lint.py
+++ lint.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from genericpath import isfile
 import os
 from colorama import init as init_colorama, Fore as ForeColor
 
@@ -15,6 +16,13 @@ def validate_dir(dir_name):
             if not os.path.exists(card_fn):
                 print(f"{ForeColor.RED}ERROR:{ForeColor.RESET} card file {card_fn} not found")
                 error = True
+        if os.path.isfile(fn_path) and fn.endswith(".card.md"):
+            card_path = fn[:-len(".card.md")]
+            if card_path == "template":
+                continue
+            full_card_path = os.path.join(dir_name, card_path)
+            if not os.path.isdir(full_card_path):
+                print(f"{ForeColor.RED}ERROR:{ForeColor.RESET} card file {full_card_path} has no corresponding path")
     return error
 
 
